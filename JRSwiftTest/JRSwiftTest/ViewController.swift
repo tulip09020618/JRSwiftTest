9fd9e18f4432abc4d2daef650e52cf9016184186
wangjinrui <wangjinrui@hongqitech.com>
2018-01-15 09:00:17 +0800

    创建swift工程

---
+++ JRSwiftTest/JRSwiftTest/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  JRSwiftTest
+//
+//  Created by hqtech on 2018/1/14.
+//  Copyright © 2018年 tulip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var someInts = [Int](repeating: 5, count: 5);
+        
+        var someVar = someInts[0]
+        someVar = 20;
+        
+        print( "第一个元素的值 \(someVar)" )
+        print( "第二个元素的值 \(someInts[1])" )
+        print( "第三个元素的值 \(someInts[2])" )
+        print(someInts)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
