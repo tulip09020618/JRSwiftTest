f98f03e6217e5d1d66c6c59ed282e44f6a0d3600
wangjinrui <wangjinrui@hongqitech.com>
2018-01-19 10:00:09 +0800

    增加生成随机数功能

--- JRSwiftTest/JRSwiftTest/JRFirstViewController.swift
+++ JRSwiftTest/JRSwiftTest/JRFirstViewController.swift
@@ -35,4 +35,49 @@ class JRFirstViewController: UIViewController {
     @IBAction func goback(_ sender: Any) {
         self .dismiss(animated: true, completion: nil);
     }
+    
+    // MARK: 生成随机数
+    @IBAction func getRandomNum(_ sender: Any) {
+        
+        print("-------------------------------")
+        var resultStr = ""
+        for _ in 0..<5 {
+            resultStr += ("\n" + createRandomNum())
+        }
+        
+        print(resultStr)
+        
+        let alertController = UIAlertController(title: "随机结果",
+                                                message: resultStr, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "好的", style: .default, handler: {
+            action in
+            print("点击了确定")
+        })
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    // MARK: 生成随机数
+    func createRandomNum() -> String {
+        var randomNum = "红球：";
+        var randomArr = [UInt32](repeating: 0, count: 6);
+        for i in 0..<6 {
+            let redNum = arc4random() % 33 + 1;
+            randomArr[i] = redNum;
+        }
+        //数组升序排序
+        randomArr.sort() { (s1, s2) -> Bool in
+            return s1 < s2
+        };
+        
+        for item in randomArr {
+            
+            randomNum += " \(String(format:"%02d", item))"
+        }
+        
+        let blueNum = arc4random() % 16 + 1;
+        randomNum += " 篮球：\(String(format:"%02d", blueNum))"
+        return randomNum;
+    }
 }
