d22c784f356d80f08b230e3915abe3995abc5cff
wangjinrui <wangjinrui@hongqitech.com>
2018-03-08 15:50:52 +0800

    创建福彩3d项目Swift版

---
+++ JRFucai3D/JRFucai3D/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JRFucai3D
+//
+//  Created by hqtech on 2018/3/8.
+//  Copyright © 2018年 tulip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
