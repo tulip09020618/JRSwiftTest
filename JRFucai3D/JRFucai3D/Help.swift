bee54544cfca0286196a4fba9f4f0e4b86bfa125
wangjinrui <wangjinrui@hongqitech.com>
2018-03-11 17:14:24 +0800

    实现独胆预测和胆码预测算法

--- JRFucai3D/JRFucai3D/Help.swift
+++ JRFucai3D/JRFucai3D/Help.swift
@@ -14,4 +14,10 @@ import UIKit
 let SCREEN_WIDTH = UIScreen.main.bounds.size.width
 let SCREEN_HEIGHT = UIScreen.main.bounds.size.height
 
-
+// 弹出提示信息
+func showMessage(title: String?, message: String?, okTitle: String?, vc: UIViewController) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    // 添加确认按钮
+    alert.addAction(UIAlertAction(title: okTitle, style: .default, handler: nil))
+    vc.present(alert, animated: true, completion: nil)
+}
--- JRFucai3D/JRFucai3D/JRPredictionViewController.swift
+++ JRFucai3D/JRFucai3D/JRPredictionViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 enum PREDICTION_TYPE: Int {
     case PREDICTION_TYPE_ONLE // 独胆
@@ -28,6 +29,9 @@ class JRPredictionViewController: UIViewController {
         }else {
             print("双胆")
         }
+        
+        // 查询最近20期开奖结果
+        queryData(20)
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,4 +50,136 @@ class JRPredictionViewController: UIViewController {
     }
     */
 
+    // MARK: 查询开奖数据
+    // nums: 要查询的总期数
+    func queryData(_ nums: Int) {
+        //http://f.apiplus.net/fc3d-20.json
+        // 拼接请求地址
+        let requestStr = "http://f.apiplus.net/fc3d-\(nums).json"
+        // 请求url
+        let requestUrl: URL! = URL(string: requestStr)
+        print("请求地址：\(requestUrl)")
+        
+        // 请求开奖数据
+        Alamofire.request(requestStr).responseJSON {response in
+            if response.error == nil {
+                // 请求成功
+                let results = response.result.value
+                // 解析数据
+                self.parseData(results!)
+            }else {
+                print("请求错误:\(response.error!)")
+                showMessage(title: "提示", message: "请求错误:\(response.error!)", okTitle: "确定", vc: self)
+            }
+        }
+    }
+    
+    // 解析开奖结果数据
+    func parseData(_ result: Any?) {
+        if result == nil {
+            print("请求结果为空")
+            showMessage(title: "提示", message: "请求结果为空", okTitle: "确定", vc: self)
+            return
+        }
+        
+        let dic = result as! Dictionary<String, AnyObject>
+        let data = dic["data"] as! Array<Dictionary<String, AnyObject>>
+        
+        if data.count == 0 {
+            print("无数据")
+            showMessage(title: "提示", message: "无数据", okTitle: "确定", vc: self)
+            return
+        }
+        
+        var queryResult = ""
+        var queryArr: Array<Array<String>>! = [Array<String>]()
+        for i in 0..<data.count {
+            let dataDic = data[i]
+            
+            // 开奖号码
+            let opencode = dataDic["opencode"] as! String
+            // 开奖时间
+            var opentime = dataDic["opentime"] as! String
+            opentime = String(opentime.prefix(10))
+//            // 开奖期数
+//            let expect = dataDic["expect"] as! String
+            // 预测结果
+            var predict: String = ""
+            if predictType == PREDICTION_TYPE.PREDICTION_TYPE_ONLE {
+                // 独胆预测
+                predict = predictWithOnly(opencode: opencode)
+            }else {
+                // 双胆预测
+                predict = predictWithDouble(opencode: opencode)
+            }
+            
+            queryResult.append("\n时间：\(opentime)，号码：\(opencode)，预测：\(predict)")
+            queryArr.append([opentime, opencode, predict])
+        }
+        
+        showMessage(title: "开奖结果\(data.count)", message: queryResult, okTitle: "确定", vc: self)
+        print(queryResult)
+        
+    }
+    
+    // MARK: 独胆预测
+    func predictWithOnly(opencode: String) -> String {
+        let arr = opencode.split(separator: ",")
+        if arr.count != 3 {
+            print("解析错误")
+            return "-"
+        }
+        
+        let a: Int! = Int(arr[0]) // 百位
+        let b: Int! = Int(arr[1]) // 十位
+        let c: Int! = Int(arr[2]) // 个位
+        
+        // 根据开奖数求预测结果
+        // 公式：上期奖号(百位乘以4+十位乘以9+个位乘以9+3)除以10，取余数
+        var s = a * 4
+        s += b * 9
+        s += c * 9
+        s %= 10
+        
+        return "\(s)"
+    }
+    
+    // MARK: 双胆预测
+    func predictWithDouble(opencode: String) -> String {
+        
+        let arr = opencode.split(separator: ",")
+        if arr.count != 3 {
+            print("解析错误")
+            return "-"
+        }
+        
+        let a: Int! = Int(arr[0]) // 百位
+        let b: Int! = Int(arr[1]) // 十位
+        let c: Int! = Int(arr[2]) // 个位
+        
+        // 根据开奖数求预测结果
+        // 公式：上期奖号(百位乘以147+十位乘以258+个位乘以369)乘以179，删除同号后取结果的最后3位
+        var s = a * 147
+        s += b * 258
+        s += c * 369
+        s *= 179
+        
+        // 取不重复的后三位
+        var numArr: Array<String> = [String]()
+        while s > 0 {
+            let n = s % 10
+            s /= 10
+            if !numArr.contains("\(n)") {
+                numArr.append("\(n)")
+            }else {
+                continue
+            }
+        }
+        
+        if numArr.count < 3 {
+            return "-"
+        }
+        return "\(numArr[numArr.count-3]),\(numArr[numArr.count-2]),\(numArr[numArr.count-1])"
+    }
+    
 }
